304a6fead4f1a7906579971350ec30d91537274f
jaynemarie3 <42786450+jaynemarie3@users.noreply.github.com>
2020-03-25 16:26:17 -0500

    About page pushed to alpha

--- mentcare-frontend/src/App.js
+++ mentcare-frontend/src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 import ClippedDrawer from "./components/ClippedDrawer";
 import { makeStyles, CssBaseline } from "@material-ui/core";
 import ApplicationBar from "./components/ApplicationBar";
-
+import About from "./components/About";
+import WelcomePage from "./components/WelcomePage";
+import PeopleCard from "./components/PeopleCard";
+import {Route, NavLink, Switch} from 'react-router-dom';
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex"
@@ -15,12 +18,36 @@ function App() {
 
   return (
     <div className="App">
-      <div className={classes.root}>
-        <CssBaseline />
-        <ApplicationBar />
-        <ClippedDrawer />
-      </div>
-    </div>
+     {//</div>/<div className={classes.root}>
+     }
+     <Route>
+     <CssBaseline />
+     <ApplicationBar />
+     <ClippedDrawer />
+     {/* <Switch>
+        <Route exact path ="/" component={About} />
+        />
+        
+     </Switch> */}
+     {/* <div className = "nav">
+     <Route exact to="/" component = {WelcomePage} />
+
+       <NavLink exact to = "/" activeClassName = "active">Home</NavLink>
+       <NavLink exact to = "/about" activeClassName = "active">About</NavLink>
+     </div>
+     <Route render = {({location}) => (
+       
+     
+       <Switch location={location}>
+      <Route exact path="/" component = {WelcomePage} />
+      <Route exact path="/About" component = {About}/>
+      </Switch> */}
+        {/* <CssBaseline />
+        // <ApplicationBar />
+        // <ClippedDrawer />
+        // <About /></Route> */}
+        </Route>     
+     </div>
   );
 }
 
--- mentcare-frontend/src/components/ClippedDrawer.js
+++ mentcare-frontend/src/components/ClippedDrawer.js
@@ -13,6 +13,7 @@ import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import { pages } from "../util/pages";
 import Box from "@material-ui/core/Box";
 import WelcomePage from "./WelcomePage";
+import About from "./About";
 
 const drawerWidth = 240;
 
@@ -60,13 +61,15 @@ export default function ClippedDrawer() {
               <ListItem
                 button
                 key={text}
-                onClick={() => {
-                  setSelectedPage(index);
-                }}
+                onClick={() => 
+                setSelectedPage(index)
+                }
               >
                 <ListItemIcon className={classes.iconMargin}>
                   {index === pages.INDIVIDUAL_CARE ? (
                     <AccessibilityIcon />
+                  ): index === pages.ABOUT ? (
+                    <About/>, <InfoIcon />
                   ) : index === pages.ADMIN_REPORTING ? (
                     <SupervisorAccountIcon />
                   ) : index === pages.ALERTS ? (
@@ -106,9 +109,13 @@ export default function ClippedDrawer() {
           <SupervisorAccountIcon />
         ) : selectedPage === pages.ALERTS ? (
           <ErrorIcon />
+        ) : selectedPage === pages.ABOUT ? (
+          <InfoIcon />, <About/> 
+
         ) : (
           <InfoIcon />
         )}
+        
       </main>
     </div>
   );
