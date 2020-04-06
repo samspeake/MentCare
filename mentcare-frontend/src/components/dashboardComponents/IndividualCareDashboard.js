84c4dbedcaabb7ca540d1a0ac0945ce33892e1d9
samspeake <sanford.6716@gmail.com>
2020-04-06 12:04:35 -0500

    Now must be signed in with rights to view the main dashboard

--- mentcare-frontend/src/components/dashboardComponents/IndividualCareDashboard.js
+++ mentcare-frontend/src/components/dashboardComponents/IndividualCareDashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Typography } from "@material-ui/core";
 import SelectPatient from "./SelectPatient";
 import MedicalReceptionistDashboard from "./MedicalReceptionistDashboard";
 import ManagerDashboard from "./ManagerDashboard";
 import NurseDashboard from "./NurseDashboard";
 import DoctorDashboard from "./DoctorDashboard";
+import { useSelector } from "react-redux";
+import { rights } from "../../util/rights";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,13 +21,26 @@ const useStyles = makeStyles(theme => ({
   },
   spacing: {
     maring: theme.spacing(1)
+  },
+  alignText: {
+    position: "absolute",
+    left: "40%",
+    top: "20%"
   }
 }));
 
 export default function IndividualCareDashboard() {
   const classes = useStyles();
+  const user = useSelector(state => state.user);
 
-  return (
+  return user.rights === rights.NONE ? (
+    <div className={classes.alignText}>
+      <Typography variant="subtitle1">
+        Please sign in using your HSA ID to access the individual care
+        dashboard.
+      </Typography>
+    </div>
+  ) : (
     <div className={classes.root}>
       <div className={classes.row}>
         <SelectPatient />
